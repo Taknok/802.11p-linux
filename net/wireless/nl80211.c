afd9cd8e949d3971805469d92aa739ca029c6e6b
Rostislav Lisovy <rostislav.lisovy@fel.cvut.cz>
2014-10-13 17:17:18 +0200

    its-g5: Since join() handles the channel setting, this is incorrect
    Signed-off-by: Rostislav Lisovy <rostislav.lisovy@fel.cvut.cz>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -1874,7 +1874,6 @@ static bool nl80211_can_set_dev_channel(struct wireless_dev *wdev)
 	 * operation to set the monitor channel if possible.
 	 */
 	return !wdev ||
-		wdev->iftype == NL80211_IFTYPE_OCB ||
 		wdev->iftype == NL80211_IFTYPE_AP ||
 		wdev->iftype == NL80211_IFTYPE_MESH_POINT ||
 		wdev->iftype == NL80211_IFTYPE_MONITOR ||
